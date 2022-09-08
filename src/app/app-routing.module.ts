4f381424d805dae5ec4eeeccd27597ecd2d2889d
FOF-T4SU <fof.t4su.sayna@gmail.com>
2022-09-08 16:58:33 +0000

    ajout des pages home et game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EShopComponent } from './components/e-shop/e-shop.component';
+import { GameComponent } from './components/game/game.component';
+import { HomeComponent } from './components/home/home.component';
+import { MonCompteComponent } from './components/mon-compte/mon-compte.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'e-shop', component: EShopComponent },
+  { path: 'mon-compte', component: MonCompteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
