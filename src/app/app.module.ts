7ea51cef8fc2c633887dab1f55c1d9154e656a93
FOF-T4SU <fof.t4su.sayna@gmail.com>
2022-09-08 14:09:35 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { EShopComponent } from './components/e-shop/e-shop.component';
+import { MonCompteComponent } from './components/mon-compte/mon-compte.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BarNavComponent } from './components/bar-nav/bar-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    EShopComponent,
+    MonCompteComponent,
+    FooterComponent,
+    BarNavComponent
   ],
   imports: [
     BrowserModule,
